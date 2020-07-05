7e74af4e5a6e716745785d5b48fe3b3124c5180a
Rafael Meira <rafaelmeira@me.com>
2020-07-05 11:47:16 -0300

    created the first model with sequelized and insert accounts harded coded

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,13 +1,25 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const { Account } = require("../models");
 
 const router = express.Router();
 
-router.get("sign-in", (req, res) => {
+const saltRounds = 10;
+
+router.get("/sign-in", (req, res) => {
   return res.json("sign-in");
 });
 
-router.get("sign-up", (req, res) => {
-  return res.json("sign-up");
+router.get("/sign-up", async (req, res) => {
+  const email = "rafaelmeira@me.com";
+  const password = "123123";
+
+  const result = await Account.create({
+    email,
+    password: bcrypt.hashSync(password, saltRounds),
+  });
+
+  return res.json(result);
 });
 
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 const authController = require("./controllers/auth");
 
-app.use("auth", authController);
+app.use("/auth", authController);
 
 app.get("/", (request, response) => {
   return response.json("Api runing...");
